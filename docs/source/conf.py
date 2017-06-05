0b1f5c6e61a2f6a2858aef98853f467bf76099aa
Benjamin Wulff <benjamin.wulff.de@ieee.org>
2017-06-05 21:38:06 +0200

    changed copyright owner

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Selfinfiltration Experiment'
-copyright = u'2016, Neurolab/UOS'
+copyright = u'2016, Motivation Lab/UOS'
 author = u'Benjamin Wulff'
 
 # The version info for the project you're documenting, acts as replacement for
